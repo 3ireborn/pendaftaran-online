9f30a08aede36a71d5bcffa123ef71a122c5c5f2
3ireborn <pakdsugie3i@gmail.com>
2025-11-09 06:11:34 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,43 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('npa-v1').then((cache) => {
-      return cache.addAll([
-        './tools/admin-smart-motivation.html',
-        './asset/npa-icon-512.png'
-      ]);
+// 🧠 NPA Motivation Service Worker v1.0
+const CACHE_NAME = 'npa-motivation-cache-v1';
+const urlsToCache = [
+  './',
+  './admin-smart-motivation-v3.5.html',
+  './manifest.json',
+  './asset/npa-icon-512.png'
+];
+
+// Install service worker dan simpan file cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching assets...');
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+// Aktifkan dan hapus cache lama
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            console.log('Menghapus cache lama:', name);
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Tangani permintaan fetch (offline support)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
